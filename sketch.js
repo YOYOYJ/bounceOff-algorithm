55e474ded625fa0dd5e38d23582f3e187a4600c1
YOYOYJ <72976142+YOYOYJ@users.noreply.github.com>
2020-10-21 16:30:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var fixedRect, movingRect;
+var circle
+var shape
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  circle = createSprite(300,100,50,50);
+  circle.debug = true ;
+  circle.velocityX=+5;
+  shape = createSprite(200,100,5,50);
+  shape.debug=true;
+  shape.velocityX=-5;
+
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bounceOff(shape,circle);
+  bounceOff(fixedRect,movingRect);
+
+  drawSprites();
+}
+
+
+function bounceOff(object1,object2) {
+  if (object1.x - object2.x < object1.width/2 + object2.width/2
+    && object2.x - object1.x < object1.width/2 + object2.width/2) {
+  object1.velocityX = object1.velocityX * (-1);
+  object2.velocityX = object2.velocityX * (-1);
+}
+if (object1.y - object2.y < object1.height/2 + object2.height/2
+  && object2.y - object1.y < object1.height/2 + object2.height/2){
+  object1.velocityY = object1.velocityY * (-1);
+  object2.velocityY = object2.velocityY * (-1);
+}
+
+}
